bc6d7af23e174986ebb4826883bbd8cc119616f7
unknown <uyiekpenelizabeth@gmail.com>
2022-01-06 23:44:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import QuickStart from "./Component/QuickStart"
-import AreaStudy from './Component/AreaStudy';
+// import logo from './logo.svg';
+// import './App.css';
+// import QuickStart from "./Component/QuickStart"
+// import AreaStudy from './Component/AreaStudy';
+import Contact from "./Component/Contact";
 
 function App() {
   return (
     <div >
-      <QuickStart/>
-      <AreaStudy/>
+      {/* <QuickStart/>
+      <AreaStudy/> */}
+      <Contact/>
     </div>
   );
 }
